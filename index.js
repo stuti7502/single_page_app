71649ceba0e5c54c5d50f6fc297f511c07aaa722
Stuti Vithlani <me@stutivithlani.com>
2023-03-14 17:17:26 +0530

    file added

---
+++ index.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+}
+
+function home(){
+    window.scrollTo(0,10);
+}
+function about(){
+    window.scrollTo(0,957);
+}
+function image(){
+    window.scrollTo(0,1960);
+}
+function cast(){
+    window.scrollTo(0,2300);
+}
